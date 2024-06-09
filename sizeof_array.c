bd54ef02ad32ee4407104919f9ca0c7bcf2e54bb
Dhiraj Gupta <157223950+Dhirajgupta440@users.noreply.github.com>
2024-06-09 12:06:46 +0530

    Delete sizeof_array.c

--- sizeof_array.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-    int arr[]={1,2,3,4,5};
-    int length=sizeof(arr)/sizeof(arr[0]);
-    printf("number of elements present in given arry:%d",length);
-    return 0;
-}
